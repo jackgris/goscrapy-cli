5f71895cf50d2da445a230ce24755f938af61e67
Gabriel Pozo <jackgris2@gmail.com>
2023-02-16 21:56:57 -0300

    add: save command will save different wholesaler data
    That will be do it in different files

---
+++ cmd/save.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2023 Gabriel Pozo <jackgris2@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// saveCmd represents the save command
+var saveCmd = &cobra.Command{
+	Use:   "save",
+	Short: "With this you have the option of save multiple wholesaler data",
+	Long:  `This will help you having differents setup for different wholesalers, so you will have the option of choose the wholesaler who you want to scrape with the config command.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("save called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(saveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// saveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// saveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
